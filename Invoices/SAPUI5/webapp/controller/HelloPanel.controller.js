4bef15c0a0a4f5511647050f4c32357db434a1c1
jaime-zamorano <yamian23@gmail.com>
2021-05-25 14:54:17 +0000

    se suben los cambiospara deploy

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -2,14 +2,16 @@
 
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.base.Log} Log
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict";
 
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
@@ -18,6 +20,20 @@ sap.ui.define([
 
 
             },
+
+            onBeforeRendering: function(){
+           
+                window.message = 'Log message - onBeforeRendering';
+                Log.info(window.message);
+                Log.error(window.message);
+
+            }, 
+
+            onAfterRendering: function(){
+
+                debugger;
+
+            }, 
 
             onShowHello: function () {
                 //read text from i18 n model 
